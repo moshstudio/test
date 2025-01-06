80a109733edd6ad8a1952f43fcc47110f19e904a
moshang <38805177+moshstudio@users.noreply.github.com>
2025-01-06 11:39:27 +0800

    Update biqu.js

--- sources/biqu.js
+++ sources/biqu.js
@@ -15,26 +15,21 @@ class BiQu extends BookExtension {
       },
     ];
     if (!type) {
-      return items.map(
-        (item) => ({
-          id: item.url,
-          type: item.name,
-          list: [],
-          page: pageNo,
-          totalPage: 1,
-          sourceId: "",
-        })
-      );
+      return items.map((item) => ({
+        id: item.url,
+        type: item.name,
+        list: [],
+        page: pageNo,
+        totalPage: 1,
+        sourceId: "",
+      }));
     }
     const item = items.find((item) => item.name === type);
-
     if (!item) return null;
-    pageNo ||= 1;
+    pageNo = pageNo || 1;
+
     const response = await this.fetch(item.url);
-    const body = new DOMParser().parseFromString(
-      await response.text(),
-      "text/html"
-    );
+    const body = new DOMParser().parseFromString(await response.text(), "text/html");
     const elements = body.querySelectorAll(".hot_sale");
     const list = Array.from(elements.values()).map((element) => {
       const a = element.querySelector("a");
@@ -56,21 +51,50 @@ class BiQu extends BookExtension {
       totalPage: 1,
     };
   }
+
   async search(keyword, pageNo) {
-    throw new Error("Method not implemented.");
+    const url = "https://m.22biqu.net/ss/";
+    const form = new FormData();
+    form.append("searchkey", keyword);
+    form.append("submit", "");
+    const response = await this.fetch(url, {
+      method: "POST",
+      body: form,
+    });
+    const body = new DOMParser().parseFromString(await response.text(), "text/html");
+
+    const elements = body.querySelectorAll(".bookbox");
+    const list = Array.from(elements.values()).map((element) => {
+      const a = element.querySelector("a");
+      const title = element.querySelector(".bookname")?.textContent || "无标题";
+      const cover = element.querySelector(".bookimg img")?.getAttribute("src") || undefined;
+      const author = element.querySelector(".author")?.textContent || undefined;
+      const update = element.querySelector(".update")?.textContent || undefined;
+      return {
+        id: a?.getAttribute("href") || this.nanoid(),
+        title,
+        author,
+        cover,
+        page: pageNo,
+        latestUpdate: update,
+        sourceId: "",
+      };
+    });
+
+    return {
+      list,
+      page: pageNo,
+      totalPage: 1,
+    };
   }
+
   async getBookDetail(item, pageNo) {
     const url = this.urlJoin(this.baseUrl, item.id);
+
     const response = await this.fetch(url);
-    const body = new DOMParser().parseFromString(
-      await response.text(),
-      "text/html"
-    );
+    const body = new DOMParser().parseFromString(await response.text(), "text/html");
     const pageElements = body.querySelectorAll("#indexselect option");
-    const chapters = Array.from(
-      { length: pageElements.length },
-      () => []
-    );
+    const chapters = Array.from({ length: pageElements.length }, () => []);
     await Promise.all(
       Array.from(pageElements.entries()).map(async ([index, element]) => {
         let url = this.urlJoin(this.baseUrl, element.getAttribute("value"));
@@ -80,14 +104,9 @@ class BiQu extends BookExtension {
           elementBody = body;
         } else {
           const response = await this.fetch(url);
-          elementBody = new DOMParser().parseFromString(
-            await response.text(),
-            "text/html"
-          );
+          elementBody = new DOMParser().parseFromString(await response.text(), "text/html");
         }
-        const elements = elementBody.querySelectorAll(
-          ".directoryArea:not([id]) a"
-        );
+        const elements = elementBody.querySelectorAll(".directoryArea:not([id]) a");
         elements.forEach((element) => {
           const a = element;
           const title = a.textContent;
@@ -107,15 +126,13 @@ class BiQu extends BookExtension {
 
     return item;
   }
+
   async getContent(item, chapter) {
     let content = "";
     let nextPageUrl = chapter.url;
     while (nextPageUrl) {
       const response = await this.fetch(nextPageUrl);
-      const body = new DOMParser().parseFromString(
-        await response.text(),
-        "text/html"
-      );
+      const body = new DOMParser().parseFromString(await response.text(), "text/html");
       const elements = body.querySelectorAll("#chaptercontent p:not([id])");
       elements.forEach((p) => {
         content += p.textContent + "\n";
